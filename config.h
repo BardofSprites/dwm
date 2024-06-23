07ca48bd1850d814d07e8b8472d7f3e182da9305
BardofSprites <bard@ramiel.attlocal.net>
2024-06-23 19:56:22 -0400

    gentoo changes

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 /* Constants */
 #define TERMINAL  "st"
 #define TERMCLASS "St"
-#define BROWSER   "librewolf"
-#define BROWSER2  "firefox"
+#define BROWSER   "librewolf-bin"
+#define BROWSER2  "firefox-bin"
 
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
@@ -17,7 +17,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "Iosevka Comfy:size=12", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "Iosevka Comfy:pixelsize=16", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#000000";
 static char normbordercolor[]       = "#1e1e1e";
 static char normfgcolor[]           = "#ffffff";
