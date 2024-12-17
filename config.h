81ca15f142644062c2d50d996ce0674aea2e673e
Daniel <89086143+BardofSprites@users.noreply.github.com>
2024-12-17 12:38:57 -0500

    decrease font

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "Iosevka Comfy:pixelsize=18", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"  };
+static char *fonts[]          = { "Iosevka Comfy:pixelsize=16", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#000000";
 static char normbordercolor[]       = "#1e1e1e";
 static char normfgcolor[]           = "#ffffff";
